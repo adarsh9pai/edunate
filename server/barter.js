1a92c9962694de15fb0d7d973719652b543fac43
jbd95 <james.brady2@mavs.uta.edu>
2019-04-14 14:56:54 +0000

    Multiple Error Resultions

--- server/barter.js
+++ server/barter.js
@@ -23,9 +23,13 @@ barter.post('/add', (request, response) =>
         promise: request.body.promise,
         hashtags: request.body.hashtags,
         isFulfilled: false,
-        datePosted: (new Date().toJSON().slice(0,10)).split('-').join('/'),
+        datePosted: (new Date().toJSON().slice(0,10)).split('_').join('/'),
         dateEnd: request.body.dateEnd
     };
+
+    newBarter.user.displayName = newBarter.user.displayName.split('/').join('_');
+
+    console.log(newBarter);
 
     const document = fStore.doc('Barter/' + newBarter.user.displayName + '-' + newBarter.id);
     document.set(newBarter).then(() => {
@@ -41,7 +45,7 @@ barter.post('/update', (request, response) => {
     
      let updatedBarter = request.body;
      updatedBarter.isFulfilled = isFulfilled(updatedBarter);
-     let document = fStore.collection("Barter").doc(updatedBarter.user.displayName + "-" + updatedBarter.id);
+     let document = fStore.collection("Barter").doc(updatedBarter.user.displayName.split('/').join('_') + "-" + updatedBarter.id);
      document.set(updatedBarter).then(() => {
 	response.json({message: "success"});
      })
@@ -56,7 +60,7 @@ barter.post('/update', (request, response) => {
 */
 barter.get('/get', (request, response) => {
 
-        let document = fStore.collection("Barter").doc(request.query.displayName + "-" + request.query.id);
+        let document = fStore.collection("Barter").doc(request.query.displayName.split('/').join('_') + "-" + request.query.id);
         document.get().then((barter) => {
             if(barter.exists)
                 response.json(barter.data());
@@ -74,20 +78,44 @@ barter.get('/getall', (request, response) => {
 
     let collection = fStore.collection("Barter");
     let result = [];
-
     collection.get().then((barters) => {
-        barters.forEach((doc) => {
-		if((!request.query.displayName) || (doc.data().user['displayName'] == request.query.displayName))
+       barters.forEach((doc) => {
+		if((!request.query.displayName) || (doc.data().user['displayName'].split('/').join('_') == request.query.displayName.split('/').join('_')))
 		{
 			result.push(doc.data());
 		}
+		return null;
 	});
         response.json({result});
 
     }).catch((error) => {
+	console.log(error.message);
         response.json({message: "Error. Try again later."});
     });
 
+
+});
+
+barter.get('/trending', (request, response) => {
+	
+	let collection = fStore.collection("Barter");
+	let hashtags = {};
+
+	collection.get().then((barters) => {
+		barters.forEach((doc) => {
+			for(let i = 0; i < doc.data().hashtags.length; i++)
+			{
+				if(!(doc.data().hashtags[i] in hashtags))
+					hashtags[doc.data().hashtags[i]] = 0;
+				hashtags[doc.data().hashtags[i]]++;
+			}
+		});
+		let sorted = Object.keys(hashtags).sort((a,b) => {return hashtags[b] - hashtags[a]});
+		response.json({hashtags: sorted});
+	})
+	.catch((error) => {
+		response.json({message: error.message});
+	});
 
 });
 
--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ const axios = require('axios');
 const configFile = require('./config');
 const stripe = require('stripe')(configFile.secret_key);
 
+app.use(function(req, res, next) {
+	       res.header("Access-Control-Allow-Origin", "*");
+	       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	          next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
