31986f34bfda80bae1862592dbea011c66751025
ImtiazKhaled <44986448+ImtiazKhaled@users.noreply.github.com>
2019-04-14 01:30:51 -0500

    payment added, logo updated

--- client/src/routes.js
+++ client/src/routes.js
@@ -6,6 +6,7 @@ import Homepage from './Homepage';
 import Profile from './Profile';
 import PageNotFound from "./Components/pageNotFound";
 import NewProfile from './Login/newProfile';
+import Payment from './Payment/Payment';
 
 
 export default (
@@ -14,6 +15,8 @@ export default (
         <Route path='/homepage' component={Homepage} exact />
         <Route path='/profile' component={Profile} exact />
         <Route path='/newProfile' component={NewProfile} exact />
+        <Route path='/payment' component={Payment} exact />
+
 
         <Route component={PageNotFound} />
     </Switch>
